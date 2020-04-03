d3c51121ed20094aca34e330953be3f5f49ef2c7
nizidara <contact@nizidara.com>
2020-04-03 15:20:46 +0900

    Herokuで起動できるように変更

---
+++ index.js
@@ -0,0 +1,62 @@
+'use strict';
+const http = require('http');
+const pug = require("pug");
+
+const server = http.createServer((req, res) => {
+
+	const now = new Date();
+	console.info(`[${now}] Requested by ${req.connection.remoteAddress}`)
+
+	res.writeHead(200, {
+		'Content-Type': 'text/html; charset=utf-8'
+	});
+
+	switch (req.method){
+		case 'GET':
+			if(req.url == "/enquetes/yaki-shabu") {
+				res.write(pug.renderFile("./form.pug", {
+					path: req.url,
+					firstItem:"焼き肉",
+					secondItem:"しゃぶしゃぶ"
+				}));
+			}else if(req.url =="/enquetes/rice-bread"){
+				res.write(pug.renderFile("./form.pug", {
+					path: req.url,
+					firstItem:"ごはん",
+					secondItem:"パン"
+				}));
+			}
+			res.end();
+			break;
+		
+		case 'POST':
+			let rawData = '';
+			req.on('data', (chunk) => {
+				rawData += chunk;
+			}).on('end', () => {
+				const decoded = decodeURIComponent(rawData);
+				console.info('[' + now + '] 投稿: ' + decoded);
+				res.write(`<!DOCTYPE html><html><body><h1>${decoded} が投稿されました</h1></body></html>`);
+				res.end();
+			});
+			
+			break;
+		
+		case 'DELETE':
+			res.write(`DELETE ${req.url}`);
+			break;
+		
+		default:
+			break;
+	}
+
+}).on('error', (e) => {
+	console.error(`[ ${new Date()} ] server error`, e);
+}).on('clientError', (e) => {
+	console.error(`[ ${new Date()} ] client error`, e);
+});
+
+const port = process.env.PORT || 8000;
+server.listen(port, () => {
+	console.log('Listening on ' + port);
+});
